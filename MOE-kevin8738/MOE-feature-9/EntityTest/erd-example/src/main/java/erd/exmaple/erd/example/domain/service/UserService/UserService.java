0d0551941474522a3f2984a926adb3b530fc8dfb
DongWooKim4343 <kd6904@naver.com>
2024-07-20 01:47:57 +0900

    PW 찾기 변경 로그인

--- MOE-kevin8738/MOE-feature-9/EntityTest/erd-example/src/main/java/erd/exmaple/erd/example/domain/service/UserService/UserService.java
+++ MOE-kevin8738/MOE-feature-9/EntityTest/erd-example/src/main/java/erd/exmaple/erd/example/domain/service/UserService/UserService.java
@@ -1,11 +1,18 @@
 package erd.exmaple.erd.example.domain.service.UserService;
 
+import erd.exmaple.erd.example.domain.dto.passwordDTO.PasswordChangeRequestDTO;
 import erd.exmaple.erd.example.domain.dto.UserPhoneNumberCheckResultDTO;
 import erd.exmaple.erd.example.domain.dto.UserRequestDTO;
 import erd.exmaple.erd.example.domain.dto.UserResponseDTO;
+import erd.exmaple.erd.example.domain.dto.passwordDTO.PasswordFindRequestDTO;
+import erd.exmaple.erd.example.domain.dto.passwordDTO.PasswordSetRequestDTO;
 
 public interface UserService {
     UserResponseDTO.JoinResultDTO joinUser(UserRequestDTO.JoinDto joinDto);
     UserPhoneNumberCheckResultDTO checkPhoneNumber(String phoneNumber);
     UserResponseDTO getUserById(Long userId);
+    String resetPasswordByPhoneNumber(String phoneNumber);
+    void changePassword(PasswordChangeRequestDTO passwordChangeRequest) throws Exception;
+    void findPassword(PasswordFindRequestDTO passwordFindRequest) throws Exception;
+    //void setNewPassword(PasswordSetRequestDTO passwordSetRequest) throws Exception;
 }
