e23229b20d63826f21c49e4224ae3d2766101553
Lee Jungmin <kevin873820@gmail.com>
2024-07-24 15:48:16 +0900

    검색API 오류 메세지 추가

--- MOE-kevin8738/MOE-feature-9/EntityTest/erd-example/src/main/java/erd/exmaple/erd/example/domain/controller/SearchController.java
+++ MOE-kevin8738/MOE-feature-9/EntityTest/erd-example/src/main/java/erd/exmaple/erd/example/domain/controller/SearchController.java
@@ -20,13 +20,16 @@ public class SearchController {
     private final SearchPageService searchService;
 
     @GetMapping("/{userId}")
-    public ResponseEntity<List<SearchResultDTO>> search(@PathVariable Long userId, @RequestParam String keyword, @RequestParam(defaultValue = "0") int page) {
+    public ResponseEntity<Object> search(@PathVariable Long userId, @RequestParam String keyword, @RequestParam(defaultValue = "0") int page) {
         List<SearchResultDTO> results = searchService.search(keyword, userId, PageRequest.of(page, 6));
+        if (results.isEmpty()) {
+            return ResponseEntity.ok("해당 전시회나 팝업스토어가 존재하지 않습니다.");
+        }
         return ResponseEntity.ok(results);
     }
 
     @GetMapping("/recent/{userId}")
-    public ResponseEntity<?> getRecentSearches(@PathVariable Long userId) {
+    public ResponseEntity<Object> getRecentSearches(@PathVariable Long userId) {
         List<SearchResultDTO> recentSearches = searchService.getRecentSearches(userId);
         if (recentSearches.isEmpty()) {
             return ResponseEntity.ok("최근 검색 기록이 없습니다.");
