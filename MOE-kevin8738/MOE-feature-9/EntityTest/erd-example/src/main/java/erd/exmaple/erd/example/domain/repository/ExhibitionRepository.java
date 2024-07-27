419ea66fc9fb0a289877e295b582469774a6ea9e
oh-pink <ohgongjuu0406@gamil.com>
2024-07-27 21:12:41 +0900

    메인페이지 최신순 팝업스토어/전시회 조회 api

--- MOE-kevin8738/MOE-feature-9/EntityTest/erd-example/src/main/java/erd/exmaple/erd/example/domain/repository/ExhibitionRepository.java
+++ MOE-kevin8738/MOE-feature-9/EntityTest/erd-example/src/main/java/erd/exmaple/erd/example/domain/repository/ExhibitionRepository.java
@@ -1,6 +1,8 @@
 package erd.exmaple.erd.example.domain.repository;
 
 import erd.exmaple.erd.example.domain.ExhibitionEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +10,5 @@ import java.util.List;
 
 public interface ExhibitionRepository extends JpaRepository<ExhibitionEntity, Long> {
     List<ExhibitionEntity> findByNameContaining(String keyword);
+    Page<ExhibitionEntity> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
--- MOE-kevin8738/MOE-feature-9/EntityTest/erd-example/src/main/java/erd/exmaple/erd/example/domain/repository/PopupStoreRepository.java
+++ MOE-kevin8738/MOE-feature-9/EntityTest/erd-example/src/main/java/erd/exmaple/erd/example/domain/repository/PopupStoreRepository.java
@@ -1,10 +1,14 @@
 package erd.exmaple.erd.example.domain.repository;
 
+
 import erd.exmaple.erd.example.domain.Popup_StoreEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PopupStoreRepository extends JpaRepository<Popup_StoreEntity, Long> {
     List<Popup_StoreEntity> findByNameContaining(String keyword);
+    Page<Popup_StoreEntity> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
